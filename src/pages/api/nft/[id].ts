1c5bbad1c97d92c003a60d9c054f83a8738581ad
Y0SH4 <yosa.agung13@gmail.com>
2024-05-20 12:33:16 +0700

    update image api

---
+++ src/pages/api/nft/[id].ts
@@ -0,0 +1,23 @@
+import { NextApiHandler } from "next";
+import { CARD_IMAGE_MAP } from "constant/image";
+
+const handler: NextApiHandler = async (req, res) => {
+  const id = req.query.id;
+  const image = CARD_IMAGE_MAP[id as "0"];
+  const host = req.headers["host"];
+  if (!image) {
+    return res
+      .status(404)
+      .json({ status: 404, message: "The requested URL was not found" });
+  }
+  const imageTemplate = {
+    name: `Bullcuan ${Number(id) + 1}`,
+    description: `NFT Bullcuan ${Number(id) + 1}`,
+    external_url: `https://${host}/api/nft/${id}`,
+    image: image,
+  };
+
+  return res.status(200).json(imageTemplate);
+};
+
+export default handler;
